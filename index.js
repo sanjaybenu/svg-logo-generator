f9ba1f4b19d06d9c7a4365a921a85ea78e5733b8
S Chopra <sanjaybenu@gmail.com>
2023-04-25 16:36:38 +1000

    added code to index file

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { Shapes, Circle, Polygon, Square } = require('./main/lib/shapes')
+const questions = require('./main/lib/questions')
+
+// Code using Switch
+
+const init = () => {
+    inquirer.prompt(questions)
+        .then((data) => {
+            console.log("Creating svg file...");
+            switch (`${data.shape}`) {
+                case 'Square':
+                    console.log('Square is being Created....')
+                    const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.width)
+                    fs.writeFile("main/output/logo.svg", square.renderSquare(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Congratulations!!! Square is now created!!');
+                        }
+                    });
+                    break;
+                case 'Circle':
+                    console.log('Circle is being Created....')
+                    const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.textColor, data.text, data.radius)
+                    fs.writeFile("main/output/logo.svg", circle.renderCircle(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Congratulations!!! Circle is now created!!');
+                        }
+                    });
+                    break;
+                case 'Triangle':
+                    console.log('Triangle is being Created....')
+                    const triangle = new Polygon(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor);
+                    fs.writeFile("main/output/logo.svg", triangle.renderPolygon(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Congratulations!!! Triangle is  now created!!')
+                        }
+                    });
+                    break;
+            }
+        });
+}
+
+init();
